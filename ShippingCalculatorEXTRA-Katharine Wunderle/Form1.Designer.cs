52e565ec20a227557de63cad2327743c9b642761
Katie <95194045+kwunderle@users.noreply.github.com>
2022-08-18 21:03:42 -0400

    Add files via upload

---
+++ ShippingCalculatorEXTRA-Katharine Wunderle/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace ShippingCalculatorEXTRA_Katharine_Wunderle
+{
+    partial class shipCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entrInfoLbl = new System.Windows.Forms.Label();
+            this.pkgWgtLbl = new System.Windows.Forms.Label();
+            this.zipCdLbl = new System.Windows.Forms.Label();
+            this.pkgWgtIpt = new System.Windows.Forms.TextBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.wgtCstLbl = new System.Windows.Forms.Label();
+            this.znCstLbl = new System.Windows.Forms.Label();
+            this.ttlCstLbl = new System.Windows.Forms.Label();
+            this.wgtCstOtpt = new System.Windows.Forms.Label();
+            this.znCstOtpt = new System.Windows.Forms.Label();
+            this.ttlCstOtpt = new System.Windows.Forms.Label();
+            this.cpdLbl = new System.Windows.Forms.Label();
+            this.nRad = new System.Windows.Forms.RadioButton();
+            this.sRad = new System.Windows.Forms.RadioButton();
+            this.eRad = new System.Windows.Forms.RadioButton();
+            this.wRad = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // entrInfoLbl
+            // 
+            this.entrInfoLbl.AutoSize = true;
+            this.entrInfoLbl.Location = new System.Drawing.Point(53, 21);
+            this.entrInfoLbl.Name = "entrInfoLbl";
+            this.entrInfoLbl.Size = new System.Drawing.Size(134, 13);
+            this.entrInfoLbl.TabIndex = 0;
+            this.entrInfoLbl.Text = "Enter Shipping Information:";
+            // 
+            // pkgWgtLbl
+            // 
+            this.pkgWgtLbl.AutoSize = true;
+            this.pkgWgtLbl.Location = new System.Drawing.Point(29, 51);
+            this.pkgWgtLbl.Name = "pkgWgtLbl";
+            this.pkgWgtLbl.Size = new System.Drawing.Size(90, 13);
+            this.pkgWgtLbl.TabIndex = 1;
+            this.pkgWgtLbl.Text = "Package Weight:";
+            this.pkgWgtLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // zipCdLbl
+            // 
+            this.zipCdLbl.AutoSize = true;
+            this.zipCdLbl.Location = new System.Drawing.Point(12, 78);
+            this.zipCdLbl.Name = "zipCdLbl";
+            this.zipCdLbl.Size = new System.Drawing.Size(107, 13);
+            this.zipCdLbl.TabIndex = 2;
+            this.zipCdLbl.Text = "Shipping Zone Code:";
+            this.zipCdLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pkgWgtIpt
+            // 
+            this.pkgWgtIpt.Location = new System.Drawing.Point(125, 48);
+            this.pkgWgtIpt.Name = "pkgWgtIpt";
+            this.pkgWgtIpt.Size = new System.Drawing.Size(62, 20);
+            this.pkgWgtIpt.TabIndex = 3;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(248, 142);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(75, 23);
+            this.calcBtn.TabIndex = 5;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // wgtCstLbl
+            // 
+            this.wgtCstLbl.AutoSize = true;
+            this.wgtCstLbl.Location = new System.Drawing.Point(280, 51);
+            this.wgtCstLbl.Name = "wgtCstLbl";
+            this.wgtCstLbl.Size = new System.Drawing.Size(68, 13);
+            this.wgtCstLbl.TabIndex = 6;
+            this.wgtCstLbl.Text = "Weight Cost:";
+            this.wgtCstLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // znCstLbl
+            // 
+            this.znCstLbl.AutoSize = true;
+            this.znCstLbl.Location = new System.Drawing.Point(245, 82);
+            this.znCstLbl.Name = "znCstLbl";
+            this.znCstLbl.Size = new System.Drawing.Size(103, 13);
+            this.znCstLbl.TabIndex = 7;
+            this.znCstLbl.Text = "Shipping Zone Cost:";
+            this.znCstLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // ttlCstLbl
+            // 
+            this.ttlCstLbl.AutoSize = true;
+            this.ttlCstLbl.Location = new System.Drawing.Point(290, 113);
+            this.ttlCstLbl.Name = "ttlCstLbl";
+            this.ttlCstLbl.Size = new System.Drawing.Size(58, 13);
+            this.ttlCstLbl.TabIndex = 8;
+            this.ttlCstLbl.Text = "Total Cost:";
+            this.ttlCstLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // wgtCstOtpt
+            // 
+            this.wgtCstOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.wgtCstOtpt.Location = new System.Drawing.Point(354, 46);
+            this.wgtCstOtpt.Name = "wgtCstOtpt";
+            this.wgtCstOtpt.Size = new System.Drawing.Size(65, 23);
+            this.wgtCstOtpt.TabIndex = 9;
+            this.wgtCstOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // znCstOtpt
+            // 
+            this.znCstOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.znCstOtpt.Location = new System.Drawing.Point(354, 77);
+            this.znCstOtpt.Name = "znCstOtpt";
+            this.znCstOtpt.Size = new System.Drawing.Size(65, 23);
+            this.znCstOtpt.TabIndex = 10;
+            this.znCstOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ttlCstOtpt
+            // 
+            this.ttlCstOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ttlCstOtpt.Location = new System.Drawing.Point(354, 108);
+            this.ttlCstOtpt.Name = "ttlCstOtpt";
+            this.ttlCstOtpt.Size = new System.Drawing.Size(65, 23);
+            this.ttlCstOtpt.TabIndex = 11;
+            this.ttlCstOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cpdLbl
+            // 
+            this.cpdLbl.AutoSize = true;
+            this.cpdLbl.Location = new System.Drawing.Point(242, 113);
+            this.cpdLbl.Name = "cpdLbl";
+            this.cpdLbl.Size = new System.Drawing.Size(0, 13);
+            this.cpdLbl.TabIndex = 12;
+            // 
+            // nRad
+            // 
+            this.nRad.AutoSize = true;
+            this.nRad.Location = new System.Drawing.Point(125, 77);
+            this.nRad.Name = "nRad";
+            this.nRad.Size = new System.Drawing.Size(33, 17);
+            this.nRad.TabIndex = 13;
+            this.nRad.TabStop = true;
+            this.nRad.Text = "N";
+            this.nRad.UseVisualStyleBackColor = true;
+            // 
+            // sRad
+            // 
+            this.sRad.AutoSize = true;
+            this.sRad.Location = new System.Drawing.Point(125, 100);
+            this.sRad.Name = "sRad";
+            this.sRad.Size = new System.Drawing.Size(32, 17);
+            this.sRad.TabIndex = 14;
+            this.sRad.TabStop = true;
+            this.sRad.Text = "S";
+            this.sRad.UseVisualStyleBackColor = true;
+            // 
+            // eRad
+            // 
+            this.eRad.AutoSize = true;
+            this.eRad.Location = new System.Drawing.Point(125, 122);
+            this.eRad.Name = "eRad";
+            this.eRad.Size = new System.Drawing.Size(32, 17);
+            this.eRad.TabIndex = 15;
+            this.eRad.TabStop = true;
+            this.eRad.Text = "E";
+            this.eRad.UseVisualStyleBackColor = true;
+            // 
+            // wRad
+            // 
+            this.wRad.AutoSize = true;
+            this.wRad.Location = new System.Drawing.Point(125, 145);
+            this.wRad.Name = "wRad";
+            this.wRad.Size = new System.Drawing.Size(36, 17);
+            this.wRad.TabIndex = 16;
+            this.wRad.TabStop = true;
+            this.wRad.Text = "W";
+            this.wRad.UseVisualStyleBackColor = true;
+            // 
+            // shipCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 177);
+            this.Controls.Add(this.wRad);
+            this.Controls.Add(this.eRad);
+            this.Controls.Add(this.sRad);
+            this.Controls.Add(this.nRad);
+            this.Controls.Add(this.cpdLbl);
+            this.Controls.Add(this.ttlCstOtpt);
+            this.Controls.Add(this.znCstOtpt);
+            this.Controls.Add(this.wgtCstOtpt);
+            this.Controls.Add(this.ttlCstLbl);
+            this.Controls.Add(this.znCstLbl);
+            this.Controls.Add(this.wgtCstLbl);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.pkgWgtIpt);
+            this.Controls.Add(this.zipCdLbl);
+            this.Controls.Add(this.pkgWgtLbl);
+            this.Controls.Add(this.entrInfoLbl);
+            this.Name = "shipCalc";
+            this.Text = "Shipping Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label entrInfoLbl;
+        private System.Windows.Forms.Label pkgWgtLbl;
+        private System.Windows.Forms.Label zipCdLbl;
+        private System.Windows.Forms.TextBox pkgWgtIpt;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Label wgtCstLbl;
+        private System.Windows.Forms.Label znCstLbl;
+        private System.Windows.Forms.Label ttlCstLbl;
+        private System.Windows.Forms.Label wgtCstOtpt;
+        private System.Windows.Forms.Label znCstOtpt;
+        private System.Windows.Forms.Label ttlCstOtpt;
+        private System.Windows.Forms.Label cpdLbl;
+        private System.Windows.Forms.RadioButton nRad;
+        private System.Windows.Forms.RadioButton sRad;
+        private System.Windows.Forms.RadioButton eRad;
+        private System.Windows.Forms.RadioButton wRad;
+    }
+}
+
---
+++ ShippingCalculatorEXTRA-Katharine Wunderle/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShippingCalculatorEXTRA_Katharine_Wunderle
+{
+    //Author: Katharine Wunderle
+    //ID: 623748
+    //Date: 03/14/2021
+    //Goal: Calculate shipping cost based on package weight and shipping zone
+    public partial class shipCalc : Form
+    {
+        //Zone price constants
+        const decimal ppp = 18m;
+        public shipCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            //Package weight variable
+            decimal pkgWgt = 0;
+            //Zone input variable
+            decimal zonePrice = 0;
+            bool tryParseResult;
+            //Output variables
+            decimal wgtCst = 0;
+            decimal ttlCst = 0;
+
+            //Clear calculation and capped labels with new calculation 
+            wgtCstOtpt.Text = "";
+            znCstOtpt.Text = "";
+            ttlCstOtpt.Text = "";
+            cpdLbl.Text = "";
+            //INPUT
+            //Get package weight and verify value from user
+            tryParseResult = decimal.TryParse(pkgWgtIpt.Text, out pkgWgt);
+            if (tryParseResult == false || pkgWgt <= 0)
+            {
+                MessageBox.Show("Error: Please enter a positive, numeric weight");
+                    return; }
+            //Get zone code from radio button
+            if (nRad.Checked == true)
+            { zonePrice = 27m; }
+            else if (sRad.Checked == true)
+            { zonePrice = 36m; }
+            else if (eRad.Checked == true)
+            { zonePrice = 45m; }
+            else if (wRad.Checked == true)
+            { zonePrice = 54m; }
+            //Show error message is no button is selected
+            else
+            { MessageBox.Show("Error: Please Pick a Zone");
+                return;
+            }
+            //CALCULATIONS
+            //Calculate weight cost
+            wgtCst = pkgWgt * ppp;
+            //Calculate total cost
+            ttlCst = wgtCst + zonePrice;
+
+            //Cap shipping cost at $100
+            if (ttlCst > 100)
+            { ttlCst = 100;
+                cpdLbl.Text = "CAPPED";
+            }
+            else
+                { cpdLbl.Text = ""; }
+
+            //OUTPUT
+            //Display weight cost
+            wgtCstOtpt.Text = wgtCst.ToString("c");
+            //Display shipping zone cost
+            znCstOtpt.Text = zonePrice.ToString("c");
+            //Display total cost
+            ttlCstOtpt.Text = ttlCst.ToString("c");
+        }
+    }
+}
